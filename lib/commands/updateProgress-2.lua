f91b0455435ad3820fd5a08170af7ff481b7f3ee
Jonathan Hulme <jon@digipigeon.com>
2020-05-27 14:30:25 +0000

    Rebase with Progress Status script

--- lib/commands/updateProgress-2.lua
+++ lib/commands/updateProgress-2.lua
@@ -7,9 +7,15 @@
       
         ARGV[1] progress
         ARGV[2] event data
+        ARGV[3] event logdata
 
       Event:
         progress(jobId, progress)
 ]]
 redis.call("HSET", KEYS[1], "progress", ARGV[1])
-redis.call("PUBLISH", KEYS[2], ARGV[2])
+if ARGV[3] ~= "" then
+	local logdata = redis.call('HGET', KEYS[1], "log")
+	if logdata == false then logdata = "" else logdata = logdata .. "\n" end
+	redis.call("HSET", KEYS[1], "log", logdata .. ARGV[3])
+end
+redis.call("PUBLISH", KEYS[2], ARGV[2] .. "|" .. ARGV[1] .. "|" .. ARGV[3])
